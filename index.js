d1624d80f2154f14dbe3e1ce7bfd7d48a417eef4
Kristof Dombi <dombi.kristof@gmail.com>
2023-12-21 17:42:30 +0100

    chore(kinsta): add splash screen

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-import { parse } from 'https://deno.land/std@0.168.0/flags/mod.ts';
 
-const { args } = Deno;
 const argPort = Deno.env.get("PORT") ? Number(Deno.env.get("PORT")) : 8000;
 
-serve((_req) => new Response("Hello, world"), { port: argPort });
+serve(
+  async (_req) => {
+    const body = (await Deno.open("index.html")).readable;
+
+    return new Response(body, {
+      headers: {
+        "content-type": 'text/html; charset=UTF-8',
+      },
+    });
+  },
+  { port: argPort },
+);
